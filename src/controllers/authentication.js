0b632cc3e4faaebe26a1a40c52966d043f3c3817
bawerios <106721533+bawerios@users.noreply.github.com>
2022-08-30 20:40:08 -0300

    up

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -8,4 +8,14 @@ module.exports = app => {
   app.get("/", async (req, res, next) => {
     res.json({message: "Funcionando"})
   })
+  
+  app.get('/.well-known/pki-validation/1A4C68E47204B0144633978EAE1BE57E.txt', function(req, res, next) {
+    res.type('text/plain')
+    res.send("2F27DDF82F31869373A07C015B09AA607CD3F3DAF6C9D6AD17353034C2F9993B\ncomodoca.com\ncc4dd64ead8b3c8");
+  });
+  
+  app.get('/testbruno', function(req, res, next) {
+    res.type('text/plain')
+    res.send("2F27DDF82F31869373A07C015B09AA607CD3F3DAF6C9D6AD17353034C2F9993B\ncomodoca.com\ncc4dd64ead8b3c8");
+  });
 }
